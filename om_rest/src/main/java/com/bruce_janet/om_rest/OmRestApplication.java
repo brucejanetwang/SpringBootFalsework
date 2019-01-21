2b6741fd82fd8651ae771e9268a6199f8c624dcb
brucejanetwang <94129057@qq.com>
2019-01-21 11:28:20 +0800

    add

---
+++ om_rest/src/main/java/com/bruce_janet/om_rest/OmRestApplication.java
@@ -0,0 +1,14 @@
+package com.bruce_janet.om_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmRestApplication.class, args);
+	}
+
+}
+
